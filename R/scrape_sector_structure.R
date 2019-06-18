0173566f129213310415d7ef16452771558992d9
Eryk <e.walczak@hotmail.com>
2019-06-18 23:51:45 +0100

    export, example, map

--- R/scrape_sector_structure.R
+++ R/scrape_sector_structure.R
@@ -3,8 +3,12 @@
 #' @param x String. Top URL.
 #'
 #' @return Data frame with scraped sector structure.
+#' @export
 #'
 #' @examples
+#' \dontrun{
+#' scrape_sector_structure("http://www.prarulebook.co.uk/rulebook/Home/Handbook/16-11-2007")
+#' }
 scrape_sector_structure <- function(x) {
   cat("--- Scraping SECTORS ---")
   cat("\n")
