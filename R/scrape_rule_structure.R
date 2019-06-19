6ea3f1958fc8eda1adf2b8d2b2aaaed5b07382e4
Eryk <e.walczak@hotmail.com>
2019-06-20 00:47:23 +0100

    date, rule id

--- R/scrape_rule_structure.R
+++ R/scrape_rule_structure.R
@@ -10,23 +10,35 @@
 #' sectors <- scrape_sector_structure("http://www.prarulebook.co.uk/rulebook/Home/Handbook/16-11-2007")
 #' parts <- scrape_part_structure(sectors)
 #' chapters <- scrape_chapter_structure(parts)
-#' scrape_rule_structure(chapters)
+#' scrape_rule_structure(chapters, "16-11-2007")
 #' }
-scrape_rule_structure <- function(df) {
+scrape_rule_structure <- function(df, date) {
   cat("\n")
   cat("--- Scraping RULES ---")
   cat("\n")
   # get all rules and append to a data frame
+  # rules <-
+  #   purrr::map_df(df[["chapter_url"]],
+  #                 scrape_menu, selector = ".rule-number")
+  # # rename
+  # colnames(rules) <- c("rule_name", "rule_url", "chapter_url")
+  #
+  # # join chapters to parts and sectors
+  # rules_chapters_parts_sectors <-
+  #   dplyr::left_join(rules, df,
+  #                    by = "chapter_url")
+  # return(rules_chapters_parts_sectors)
+
+  # new method - extract IDs - allows getting rule URLs
+  # get all rules and append to a data frame
   rules <-
     purrr::map_df(df[["chapter_url"]],
-                  scrape_menu, selector = ".rule-number")
-  # rename
-  colnames(rules) <- c("rule_name", "rule_url", "chapter_url")
+                  scrape_menu, selector = "a", date = date)
 
-  # join chapters to parts and sectors
   rules_chapters_parts_sectors <-
     dplyr::left_join(rules, df,
                      by = "chapter_url")
 
   return(rules_chapters_parts_sectors)
+
 }
