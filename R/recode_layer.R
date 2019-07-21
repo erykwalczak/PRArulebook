8b649f20efc59dd5721bfafa9bd71fa9384b662d
Eryk <e.walczak@hotmail.com>
2019-07-21 23:06:16 +0100

    validate from. join on one column

--- R/recode_layer.R
+++ R/recode_layer.R
@@ -1,7 +1,8 @@
 #' Recode layer of the rulebook
 #'
 #' @param from Source layer. Rule, chapter, part, sector.
-#' @param to Target layer. Rule, chapter, part, sector.
+#' @param to Target layer. Available options: rule_url, rule_name,
+#' chapter_url, chapter_name, part_url, part_name, sector_url, sector_name, all
 #' @param structure_file Data frame. Output of \code{"get_structure"}.
 #'
 #' @return Data frame with recoded layers.
@@ -10,30 +11,31 @@
 #' @examples
 #' \dontrun{
 #' rulebook_structure <- get_structure("16-11-2017")
+#'
 #' recode_layer("http://www.prarulebook.co.uk/rulebook/Content/Chapter/211140/16-11-2017",
 #' to = "all", structure_file = rulebook_structure)
+#'
+#' recode_layer(rulebook_structure$part_url[2], "part_name", rulebook_structure)
 #' }
 recode_layer <- function(from, to = "all", structure_file) {
 
-  # validate to type
+  # validate 'to' type
   if (!(to %in% c("rule_url", "rule_name", "chapter_url", "chapter_name",
-  "part_url", "part_name", "sector_url", "sector_name", "all"))) {
+                  "part_url", "part_name", "sector_url", "sector_name", "all"))) {
     stop("Provide correct 'to' argument. Available options: rule_url, rule_name,
          chapter_url, chapter_name, part_url, part_name, sector_url, sector_name, all.")
   }
 
+  # extract link type from the url
   assign_link_type <- function(x) {
-    ifelse(is.na(x), NA,
+    ifelse(is.na(x), stop("Provide correct 'from' URL"),
            ifelse(grepl("Content/Part", x), "Part",
                   ifelse(grepl("Content/Chapter", x), "Chapter",
                          ifelse(grepl("Content/Rule", x), "Rule",
                                 ifelse(grepl("Content/Sector", x), "Sector",
-                                       ifelse(grepl("LegalInstrument", x), "Legal",
-                                              ifelse(grepl("/Glossary", x), "Glossary",
-                                                     "Other")))))))
+                                                     "Other")))))
   }
-
-  # check the type of link
+  # check the type of a 'from' link
   from_type <- assign_link_type(from)
   # assign appropriate variable name
   from_type_url <- ifelse(from_type == "Sector", "sector_url",
@@ -42,20 +44,30 @@ recode_layer <- function(from, to = "all", structure_file) {
                                         ifelse(from_type == "Rule", "rule_url",
                                                stop("Incorrect URL")))))
 
+  # check if 'from' column is in the structure file
+  if (!(from_type_url %in% names(structure_file))) {
+    stop("Check if 'from' argument was can be found in the 'structure_file'.")
+  }
+
   from_df <- data.frame(from, stringsAsFactors = FALSE)
   # rename the column to match the structure file
   colnames(from_df) <- from_type_url
 
-  # or find in data.frame if faster ???
-
+  # pull all columns from the structure file
   if (to == "all") {
     joined_layers <-
       dplyr::left_join(from_df, structure_file)
+
   } else {
+
+    # choose columns to subset
+    cols2select <- c(from_type_url, to)
+    # subset the df to keep only from-to columns
+    df_selected <- structure_file[, cols2select]
+
     joined_layers <-
-      dplyr::left_join(from, structure_file[[c(from_type_url, to)]])
+      dplyr::left_join(from_df, df_selected)
   }
 
   return(joined_layers)
-  # TODO throw warnings
 }
