d3e9d20c6514c7c351df384fba579f8269417d78
Eryk Walczak <erykjwalczak@gmail.com>
2019-10-29 15:59:18 +0000

    test links

--- tests/testthat/test-get_content.R
+++ tests/testthat/test-get_content.R
@@ -13,4 +13,15 @@ test_that("get_content works", {
     c("rule_number", "rule_text", "rule_date", "url", "active")
   returned_names <- names(one_rule)
   expect_identical(expected_names, returned_names)
+
+  # test if links are scraped
+  links_content <-
+    get_content(
+    "http://www.prarulebook.co.uk/rulebook/Content/Chapter/242047/16-11-2007",
+    "links")
+  expect_s3_class(links_content, "data.frame")
+  # test the column names
+  expected_names_links <- c("from", "to", "to_text", "to_type")
+  returned_names_links <- names(links_content)
+  expect_identical(expected_names_links, returned_names_links)
 })
