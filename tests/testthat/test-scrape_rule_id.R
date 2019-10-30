e3c2cf54ea356e239b578b77e313231bedc7b35b
Eryk Walczak <erykjwalczak@gmail.com>
2019-10-30 13:33:07 +0000

    check if NA returned

--- tests/testthat/test-scrape_rule_id.R
+++ tests/testthat/test-scrape_rule_id.R
@@ -16,8 +16,14 @@ test_that("scrape_rule_id works", {
   rule_id <-
     scrape_rule_id(rule_url, rule_no, rule_text)
   expect_s3_class(rule_id, "data.frame")
+  # assign NAs when blank url
+  rule_id_blank <-
+    scrape_rule_id(NA, rule_no, rule_text)
+  expect_s3_class(rule_id_blank, "data.frame")
+  returned_names_blank <- names(rule_id_blank)
   # check the names
   expected_names <- c("rule_number", "rule_text", "rule_url" )
   returned_names <- names(rule_id)
   expect_identical(expected_names, returned_names)
+  expect_identical(expected_names, returned_names_blank)
 })
